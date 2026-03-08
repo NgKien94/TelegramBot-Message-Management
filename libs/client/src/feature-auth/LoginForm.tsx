3625f46f9e7c59661d68e77450f648669d086f7d
Chative Kien <kien.nguyen@chative.io>
2026-03-08 13:49:16 +0700

    chore: update UI Login form

--- libs/client/src/feature-auth/LoginForm.tsx
+++ libs/client/src/feature-auth/LoginForm.tsx
@@ -5,8 +5,8 @@ export function LoginForm() {
     <form className="w-3/4 flex flex-col justify-center items-center gap-y-3">
       <Input label="Email" type="email" />
       <Input label="Password" type="password" />
-      <button className="block w-full mt-2 rounded px-3 py-2 bg-[var(--primary-color)] text-white">
-        Submit
+      <button className="block w-full mt-2 rounded px-3 py-2 bg-[var(--primary-color)] text-white hover:opacity-80 transition-all duration-200 ease-linear">
+        Login
       </button>
     </form>
   );
