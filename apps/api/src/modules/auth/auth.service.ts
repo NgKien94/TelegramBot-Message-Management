fae84956e58a2fcdb8ebf0cdfd981500a652741e
Chative Kien <kien.nguyen@chative.io>
2026-03-16 14:17:46 +0700

    refactor: replace async jwt functions to sync functions

--- apps/api/src/modules/auth/auth.service.ts
+++ apps/api/src/modules/auth/auth.service.ts
@@ -1,10 +1,5 @@
 import { PrismaService } from '@message-management/db';
-import {
-  BadRequestException,
-  Injectable,
-  NotFoundException,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { hash, verify } from 'argon2';
@@ -14,28 +9,22 @@ import { RegisterDto } from './auth.dto';
 
 @Injectable()
 export class AuthService {
+  private access_token_secret: string;
+  private access_token_expires_in: string;
+  private refresh_token_token_secret: string;
+  private refresh_token_expires_in: string;
+
   constructor(
     private readonly prismaService: PrismaService,
     private readonly jwtService: JwtService,
     private readonly configService: ConfigService,
-  ) {}
-
-  getJwtInfo() {
-    return {
-      access_token_secret: this.configService.get<string>(
-        'JWT_ACCESS_TOKEN_SECRET',
-      ),
-      access_token_expires_in: this.configService.get<string>(
-        'JWT_ACCESS_TOKEN_EXPIRES_IN',
-      ),
-      refresh_token_token_secret: this.configService.get<string>(
-        'JWT_REFRESH_TOKEN_SECRET',
-      ),
-      refresh_token_expires_in: this.configService.get<string>(
-        'JWT_REFRESH_TOKEN_EXPIRES_IN',
-      ),
-    };
+  ) {
+    this.access_token_secret = this.configService.get<string>('JWT_ACCESS_TOKEN_SECRET');
+    this.access_token_expires_in = this.configService.get<string>('JWT_ACCESS_TOKEN_EXPIRES_IN');
+    this.refresh_token_token_secret = this.configService.get<string>('JWT_REFRESH_TOKEN_SECRET');
+    this.refresh_token_expires_in = this.configService.get<string>('JWT_REFRESH_TOKEN_EXPIRES_IN');
   }
+
 
   async login(email: string, password: string) {
     const account = await this.prismaService.account.findUnique({
@@ -53,28 +42,19 @@ export class AuthService {
       throw new UnauthorizedException('Incorrect email or password');
     }
 
-    const {
-      access_token_secret,
-      access_token_expires_in,
-      refresh_token_token_secret,
-      refresh_token_expires_in,
-    } = this.getJwtInfo();
-
     const tokenPayload: TokenPayload = {
       accountId: account.id,
     };
 
-    const [access_token, refresh_token] = await Promise.all([
-      this.jwtService.signAsync(tokenPayload, {
-        secret: access_token_secret,
-        expiresIn: access_token_expires_in as StringValue,
-      }),
-      this.jwtService.signAsync(tokenPayload, {
-        secret: refresh_token_token_secret,
-        expiresIn: refresh_token_expires_in as StringValue,
-      }),
-    ]);
+    const access_token = this.jwtService.sign(tokenPayload, {
+      secret: this.access_token_secret,
+      expiresIn: this.access_token_expires_in as StringValue,
+    });
 
+    const refresh_token = this.jwtService.sign(tokenPayload, {
+      secret: this.refresh_token_token_secret,
+      expiresIn: this.refresh_token_expires_in as StringValue,
+    });
     // save refresh_token into Database
     await this.prismaService.authToken.create({
       data: {
@@ -109,27 +89,19 @@ export class AuthService {
       },
     });
 
-    const {
-      access_token_secret,
-      access_token_expires_in,
-      refresh_token_token_secret,
-      refresh_token_expires_in,
-    } = this.getJwtInfo();
-
     const tokenPayload: TokenPayload = {
       accountId: account.id,
     };
 
-    const [access_token, refresh_token] = await Promise.all([
-      this.jwtService.signAsync(tokenPayload, {
-        secret: access_token_secret,
-        expiresIn: access_token_expires_in as StringValue,
-      }),
-      this.jwtService.signAsync(tokenPayload, {
-        secret: refresh_token_token_secret,
-        expiresIn: refresh_token_expires_in as StringValue,
-      }),
-    ]);
+    const access_token = this.jwtService.sign(tokenPayload, {
+      secret: this.access_token_secret,
+      expiresIn: this.access_token_expires_in as StringValue,
+    });
+
+    const refresh_token = this.jwtService.sign(tokenPayload, {
+      secret: this.refresh_token_token_secret,
+      expiresIn: this.refresh_token_expires_in as StringValue,
+    });
 
     // save refresh_token into Database
     await this.prismaService.authToken.create({
@@ -157,19 +129,11 @@ export class AuthService {
   }
 
   async refreshToken(refresh_token: string) {
-    const {
-      access_token_secret,
-      access_token_expires_in,
-      refresh_token_token_secret,
-    } = this.getJwtInfo();
 
     try {
-      const payload = await this.jwtService.verifyAsync<TokenPayload>(
-        refresh_token,
-        {
-          secret: refresh_token_token_secret,
-        },
-      );
+      const payload = this.jwtService.verify<TokenPayload>(refresh_token, {
+        secret: this.refresh_token_token_secret,
+      });
 
       const { accountId } = payload;
       const isExistToken = await this.prismaService.authToken.findFirst({
@@ -196,9 +160,10 @@ export class AuthService {
       const tokenPayload: TokenPayload = {
         accountId: account.id,
       };
-      const access_token = await this.jwtService.signAsync(tokenPayload, {
-        secret: access_token_secret,
-        expiresIn: access_token_expires_in as StringValue,
+
+      const access_token = this.jwtService.sign(tokenPayload, {
+        secret: this.access_token_secret,
+        expiresIn: this.access_token_expires_in as StringValue,
       });
 
       return {
